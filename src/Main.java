ad439940a13f6aa7639e26f1416e1448b31b41ea
Vinicius Vilela <109028912+ViniciusMVilela@users.noreply.github.com>
2023-07-31 20:34:32 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,15 @@
 import entities.Person;
 public class Main {
     public static void main(String[] args) {
-       // Scanner sc = new Scanner(System.in);
-
+ 
         Person person = new Person();
+     
         person.setName("Vinicius Vilela");
         person.setAge(19);
         person.setDateOfBirth("22/01/2004");
+     
         System.out.println(person.getName());
         System.out.println(person);
-     //
+     
     }
 }
